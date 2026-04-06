66701957b13afd658d9d82a5f236563e4ca4bbee
PetersonSantos9K <petersonsantos9k@gmail.com>
2026-04-05 23:04:04 -0300

    Novo desafio de lógica

---
+++ src/ExerciciosBasicos/set/ex12/Program.java
@@ -0,0 +1,16 @@
+package ExerciciosBasicos.set.ex12;
+
+public class Program {
+
+    void main(){
+
+        Post post = new Post("Axios");
+
+        post.AddTag("falha");
+        post.AddTag("segurança");
+        post.listTags();
+
+    }
+
+
+}
---
+++ src/ExerciciosBasicos/set/ex13/Program.java
@@ -0,0 +1,34 @@
+package ExerciciosBasicos.set.ex13;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Program {
+
+    void main(){
+
+        Set<Integer> numbers = new TreeSet<>();
+
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(7);
+
+        Set<Integer> missingNumbers = new TreeSet<Integer>();
+        Integer previous = null;
+        for(Integer number : numbers){
+
+            if(previous != null){
+                while (previous + 1 < number){
+                    missingNumbers.add(previous+1);
+                    previous++;
+                }
+            }
+            previous = number;
+        }
+
+        for(int number : missingNumbers){
+            IO.println(number);
+        }
+    }
+}
